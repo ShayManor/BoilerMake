75bd1c983a7e493d2bac031f27950c9e36c2016b
shay <shay.manor@gmail.com>
2025-02-23 17:29:26 -0500

    Test

--- Helpers/Creator/Ask_AI-Options/Gemini.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-
-
-def ask_ai(prompt: str):
-    client = genai.Client()
-    response = client.models.generate_content(
-        model="gemini-2.0-flash", contents=prompt
-    )
-    print(response.text)
